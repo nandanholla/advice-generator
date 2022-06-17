d2f8d7931a4c535c056fb90e2976f8b2ae04ec18
nandanholla <59246994+nandanholla@users.noreply.github.com>
2022-06-17 14:12:40 +0000

    Deploying to gh-pages from @ nandanholla/advice-generator@ac3dcdcc96e91aa494a8e3a72009d4a47d2cc968 🚀

---
+++ _next/static/7O2U8N3N4AKHnHK69QPer/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-212697c3f5316b42.js"],"/_error":["static/chunks/pages/_error-0a004b8b8498208d.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
